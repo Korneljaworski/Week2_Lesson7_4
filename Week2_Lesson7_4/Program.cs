7bbb6dd2d7df213c44cadf8b6750b5f78ec9af61
Korjaw Korjaw <kornel.jaworski@gmail.com>
2023-07-28 01:39:49 +0200

    Add project files.

---
+++ Week2_Lesson7_4/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+            Napisz program w C#, który sprawdzi czy podany przez użytkownika jest rokiem przestępnym.
+            Dane testowe : 2016
+            Rezultat w terminalu :    2016 jest rokiem przestępnym
+        */
+
+        Console.WriteLine("Podaj rok:");
+
+        int chosenYear = Int32.Parse(Console.ReadLine());
+
+        if ((chosenYear % 4 == 0 && chosenYear % 100 != 0) || chosenYear % 400 == 0)
+        {
+            Console.WriteLine($"{chosenYear} jest rokiem przestępnym");
+        }
+        else
+        {
+            Console.WriteLine($"{chosenYear} nie jest rokiem przestępnym");
+        }
+    }
+
+}
